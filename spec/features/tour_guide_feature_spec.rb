25c1e0c7c3a76648a16cfeff4523a158474cfba5
Marcus <marcusbullock@hotmail.co.uk>
2016-03-22 13:19:35 +0000

    Began tour guide functionality (on wrong branch)

--- spec/features/tour_guide_feature_spec.rb
+++ spec/features/tour_guide_feature_spec.rb
@@ -6,10 +6,10 @@ feature 'Submitting an advertisement for a tour' do
     signup
   end
 
-  it '-> allows the tour guide to advertise a tour' do
+  xit '-> allows the tour guide to advertise a tour' do
     add_tour
     expect(page).to have_content('A fun history tour')
     expect(page).to have_content('Location: London')
     expect(page).to have_content('Cost: £20')
   end
-end 
+end
---
+++ spec/features/user_request_feature_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+feature 'Submitting an advertisement for a tour' do
+
+  before do
+    signup
+  end
+end 
