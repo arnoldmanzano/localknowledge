2c92a3dd9ccafd2fd27c614c0fc57e6238f086c1
ViolaCrellin <viola.crellin@gmail.com>
2016-03-27 13:01:06 +0100

    Tested request.build_reply method in request model

--- spec/factories/replies.rb
+++ spec/factories/replies.rb
@@ -1,15 +1,3 @@
-# FactoryGirl.define do
-#   factory :reply do
-#     meeting_point "MyString"
-#     duration 1
-#     cost 1
-#     stopoffs "MyText"
-#     description "MyText"
-#     request
-#     association :user_id, factory: :user
-#   end
-# end
-
 FactoryGirl.define do
 
   # reply factory with a `belongs_to` association to the user and request
--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Request, type: :model do
   let(:pending_request) {described_class}
-  let(:dummy_reply) {double :dummy_reply}
 
   it { should belong_to :user }
   it { should have_many(:replies).dependent(:destroy) }
+
+  it { is_expected.to respond_to(:set_expiration_date, :build_reply) }
 
 
   describe '#set_expiration_date' do
@@ -20,10 +21,29 @@ RSpec.describe Request, type: :model do
     it 'sets the expiration date for replied requests to 1000 days' do
       expires_on = Date.today + 1000
       request = build(:request)
-      request.build_reply(attributes_for(:reply), create(:user_with_requests))
+      request.build_reply(create(:user_with_requests))
       request.save
       expect(request.expiration).to eq(expires_on)
     end
+
+  end
+
+  describe '#build_reply' do
+
+    it 'creates a reply' do
+      request = build(:request)
+      user = create(:user_with_requests)
+      expect(request.build_reply(user)).to eq(request.replies[0])
+    end
+
+    it 'associates this reply with a user' do
+      request = build(:request)
+      user = create(:user_with_requests)
+      reply = request.build_reply(user)
+      expect(user.id).to eq(reply.user_id)
+    end
+
+
 
   end
 end
