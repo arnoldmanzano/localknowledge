1c2932eb796471f55c5c215345d34445819152a7
ViolaCrellin <viola.crellin@gmail.com>
2016-03-27 13:26:22 +0100

    Tested the reply_spec model

--- spec/models/reply_spec.rb
+++ spec/models/reply_spec.rb
@@ -3,4 +3,13 @@ require 'rails_helper'
 RSpec.describe Reply, type: :model do
   it { should belong_to :user }
   it { should belong_to :request }
+
+  describe '#find_user' do
+
+    it 'matches the username with the user id of the user who made the reply' do
+      user = create(:user_with_replies)
+      reply = create(:reply)
+      expect(reply.find_user(reply.user_id)).to eq(user.username)
+    end
+  end
 end
