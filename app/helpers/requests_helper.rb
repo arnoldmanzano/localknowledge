7a4fae33e10309f6c366fb86ce326794aee60cde
ViolaCrellin <viola.crellin@gmail.com>
2016-03-27 00:01:13 +0000

    Added more tests to rails backend

--- app/helpers/requests_helper.rb
+++ app/helpers/requests_helper.rb
@@ -1,14 +1,16 @@
 module RequestsHelper
-  def set_expiration_date
-    if self.replies.any?
-      self.expiration =  Date.today + 1000.days
-    else
-      self.expiration =  Date.today + 5.days
-    end
-  end
-
-  def build_reply(attributes = {}, user)
-    attributes[:user] ||= user
-    replies.build(attributes)
-  end
+  #Could we put this logic in here rather than in our model? Not sure if the
+  #helper modules are meant to be used for the controller only?
+  # def set_expiration_date
+  #   if self.replies.any?
+  #     self.expiration =  Date.today + 1000.days
+  #   else
+  #     self.expiration =  Date.today + 5.days
+  #   end
+  # end
+  #
+  # def build_reply(attributes = {}, user)
+  #   attributes[:user] ||= user
+  #   replies.build(attributes)
+  # end
 end
--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -1,7 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Request, type: :model do
-  it { should belong_to :user }
-  it { should have_many :replies }
+  let(:pending_request) {described_class}
+  let(:dummy_reply) {double :dummy_reply}
+  let(:dummy_user) {double :dummy_user}
 
+  it { should belong_to :user }
+  it { should have_many(:replies).dependent(:destroy) }
+
+
+  describe '#set_expiration_date' do
+
+    it 'sets the expiration date for unreplied requests to 5 days' do
+      expires_on = Date.today + 5
+      a_request = pending_request.create(id: 1, location: "Peckham", user_id: 2)
+      a_request.set_expiration_date
+      expect(a_request.expiration).to eq(expires_on)
+    end
+
+    it 'sets the expiration date for replied requests to 1000 days' do
+      expires_on = Time.now + 1000
+      a_request = pending_request.create(id: 1, location: "Peckham", user_id: 2)
+      a_request.build_reply({id: 3 }, dummy_user)
+      a_request.set_expiration_date
+      expect(a_request.expiration).to_eq(expires_on)
+    end
+
+  end
 end
