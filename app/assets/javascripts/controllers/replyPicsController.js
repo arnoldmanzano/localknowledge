2ac89b31c53616234b349210ee040caae2a090a7
arnoldmanzano <arnold.manzano@gmail.com>
2016-03-29 15:10:02 +0100

    Installing ngGallery

---
+++ app/assets/javascripts/controllers/replyPicsController.js
@@ -0,0 +1,20 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('LocalKnowledgeApp')
+    .controller('ReplyPicsController', ['$http', function($http) {
+
+    var self = this;
+
+    self.images = [
+      {thumb: 'images/thumbs/1.jpg', img: 'images/1.jpg'},
+          {thumb: 'images/thumbs/2.jpg', img: 'images/2.jpg'},
+          {thumb: 'images/thumbs/3.jpg', img: 'images/3.jpg'},
+          {thumb: 'images/thumbs/4.jpg', img: 'images/4.jpg'},
+          {thumb: 'images/thumbs/5.jpg', img: 'images/5.jpg'},
+          {thumb: 'images/thumbs/6.jpg', img: 'images/6.jpg'}
+    ];
+
+  }]);
+}());
