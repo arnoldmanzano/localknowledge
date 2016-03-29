806661d86ec11e38c0e2718e3947bc9ea5b5d314
arnoldmanzano <arnold.manzano@gmail.com>
2016-03-29 17:50:55 +0100

    Cleanup

--- app/assets/javascripts/pic_preview.js
+++ app/assets/javascripts/pic_preview.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
+    function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        $('#signUpAvatar').attr('src', e.target.result);
+      };
+    reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+  $("#imgInp").change(function() {
+    readURL(this);
+  });
+
   function readURLReplypics(input) {
   var reader = new FileReader();
   if (input.files && input.files[0]) {
@@ -9,6 +23,7 @@ $(document).ready(function() {
     reader.readAsDataURL(input.files[0]);
     }
   }
+  
   $("#replyPicsInp").change(function() {
     readURLReplypics(this);
   });
