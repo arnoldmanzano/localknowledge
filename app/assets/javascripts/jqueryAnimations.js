d8cddb5ad5f7242d14e91af28cdad039a916bee9
arnoldmanzano <arnold.manzano@gmail.com>
2016-03-31 14:40:57 +0100

    Precompiled assets

--- app/assets/javascripts/jqueryAnimations.js
+++ app/assets/javascripts/jqueryAnimations.js
@@ -29,4 +29,47 @@ $( document ).ready(function () {
   setTimeout(function(){
     $('.alert').fadeOut();
   }, 3000);
+
+  function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      $('#signUpAvatar').attr('src', e.target.result);
+    };
+  reader.readAsDataURL(input.files[0]);
+  }
+}
+
+$("#imgInp").change(function() {
+  readURL(this);
+});
+
+function readURLReplypics(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      $('#replyPicsOut1').attr('src', e.target.result);
+      $('#replyPicsOut1').attr('class', 'img-responsive');
+    };
+  reader.readAsDataURL(input.files[0]);
+  }
+}
+$("#replyPicsInp").change(function() {
+  readURLReplypics(this);
+});
+
+// function readURLReplypics(input) {
+//   if (input.files && input.files[0]) {
+//     for(var i=0; i<input.files.length; i++) {
+//       var reader = new FileReader();
+//       reader.onload = function (e) {
+//         $('#replyPics').append("<img src='" + e.target.result + "' class='img-thumbnail'/>");
+//       };
+//       reader.readAsDataURL(input.files[i]);
+//     }
+//   }
+// }
+// $("#replyPicsInp").change(function() {
+//   readURLReplypics(this);
+// });
 });
