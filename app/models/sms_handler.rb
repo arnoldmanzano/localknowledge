bd011ee195489efbcf7d5cbacee89ccb2f00afe4
arnoldmanzano <arnold.manzano@gmail.com>
2016-03-31 10:23:26 +0100

    Refactor smshandler test and model

--- app/models/sms_handler.rb
+++ app/models/sms_handler.rb
@@ -3,23 +3,22 @@ class SmsHandler
   def initialize
     @account_sid = ENV['ACCOUNT_SID']
     @auth_token = ENV['AUTH_TOKEN']
+    @twilio_phone = ENV['TWILIO_PHONE']
   end
 
   def send_you_have_reply(user)
     @client = Twilio::REST::Client.new @account_sid, @auth_token
     @client.account.messages.create({
-      :from => ENV['TWILIO_PHONE'],
+      :from => @twilio_phone,
       :to => user.phone,
       :body => 'Your request has been replied to, please visit LocalHost.com',
     })
   end
 
   def send_tour_accepted(user)
-    account_sid = ENV['ACCOUNT_SID']
-    auth_token = ENV['AUTH_TOKEN']
     @client = Twilio::REST::Client.new @account_sid, @auth_token
     @client.account.messages.create({
-      :from => ENV['TWILIO_PHONE'],
+      :from => @twilio_phone,
       :to => user.phone,
       :body => 'A user has accepted your tour request, please go online to check details.',
     })
