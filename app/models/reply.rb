4b163414536e061dfd7728f179541340b6b239a9
arnoldmanzano <arnold.manzano@gmail.com>
2016-03-31 10:33:24 +0100

    Removed some not needed twilio code

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -15,34 +15,6 @@ class Reply < ActiveRecord::Base
     self.request.save
   end
 
-  # account_sid = 'AC6bc135c3726740e0fadb9003ef1b44e4'
-  # auth_token = '6269d5a1cd3d61b469972941ad95f9cb'
-  #
-  # # set up a client to talk to the Twilio REST API
-  # @client = Twilio::REST::Client.new account_sid, auth_token
-  #
-  # # alternatively, you can preconfigure the client like so
-  # Twilio.configure do |config|
-  #   config.account_sid = account_sid
-  #   config.auth_token = auth_token
-  # end
-  #
-  # # and then you can create a new client without parameters
-  # @client = Twilio::REST::Client.new
-  #
-  # # def send_text(message)
-  # #   account_sid = '6269d5a1cd3d61b469972941ad95f9cb',
-  # #   auth_token = '6269d5a1cd3d61b469972941ad95f9cb',
-  # #   @client = Twilio::REST::Client.new account_sid, auth_token
-  # #   @message = @client.messages.create(
-  # #   to: '07791621716',
-  # #   from: '+15005550006' ,
-  # #   body: message
-  # #   )
-  # # end
-  #
-  # Twilio phone: +441945232061
-
   def message
     message = "Your tour has been chosen, please visit /replies"
   end
